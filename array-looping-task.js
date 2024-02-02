aae9f8cfdfd2db37713edfb48a9a0696d7c340cc
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-02 09:54:47 +0600

    first task compliting

--- array-looping-task.js
+++ array-looping-task.js
@@ -12,6 +12,8 @@ Write a JavaScript code to reverse the array colors `without using the reverse m
 
 */
 
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+//console.log(colors.reverse());
 
 
 /*
